f0b8dfa1f0e768ccb5bcd6ddc1b4c131ae941a36
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 23:56:05 -0600

    fix: Add categories to ransackable associations for Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   end
 
   def self.ransackable_associations(auth_object = nil)
-    [ "image_attachment", "image_blob", "cart_items", "categorizations" ]
+    [ "image_attachment", "image_blob", "cart_items", "categorizations", "categories" ]
   end
 
   has_one_attached :image
