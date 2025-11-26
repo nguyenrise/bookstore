3435d4f6a178e8f9a4d6fbbe586b6c55a9eb2a8e
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 22:26:06 -0600

    feat: Implement image uploading for products

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Book do
-  permit_params :title, :author, :description, :price, :stock_quantity
+  permit_params :title, :author, :description, :price, :stock_quantity, :image
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :author
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     [ "author", "created_at", "description", "id", "id_value", "price", "stock_quantity", "title", "updated_at" ]
   end
+
+  has_one_attached :image
 end
