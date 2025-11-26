cd3cc49d5f7dd5640bb61ae1c6653e9838e28cbc
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 22:23:36 -0600

    fix: Add ransackable_attributes to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    [ "author", "created_at", "description", "id", "id_value", "price", "stock_quantity", "title", "updated_at" ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :books, only: [:index, :show]
+  resources :books, only: [ :index, :show ]
   root "books#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
