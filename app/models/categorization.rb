b30f3aacc9d9aeac6aeda4fd2f1c2719620f3f61
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 23:54:42 -0600

    fix: Add ransackable attributes to Categorization

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,4 +1,8 @@
 class Categorization < ApplicationRecord
   belongs_to :book
   belongs_to :category
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["book_id", "category_id", "created_at", "id", "updated_at"]
+  end
 end
