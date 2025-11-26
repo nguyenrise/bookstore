500ba09e2f24f6395a9ff87f467d75fdc374353f
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 23:52:24 -0600

    fix: Add ransackable methods to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   has_many :categorizations, dependent: :destroy
   has_many :books, through: :categorizations
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["books", "categorizations"]
+  end
 end
