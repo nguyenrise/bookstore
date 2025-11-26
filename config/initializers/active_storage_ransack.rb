743a28670e15159f5165009dc3a2775e7e208e7d
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 22:30:50 -0600

    fix: Add ransackable_attributes for ActiveStorage::Attachment

---
+++ config/initializers/active_storage_ransack.rb
@@ -0,0 +1,8 @@
+# config/initializers/active_storage_ransack.rb
+Rails.application.config.to_prepare do
+  ActiveStorage::Attachment.class_eval do
+    def self.ransackable_attributes(auth_object = nil)
+      %w[blob_id created_at id name record_id record_type]
+    end
+  end
+end
