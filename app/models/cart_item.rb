44642217613e8b7f120578985bcd639c0cbd2c45
Rise Nguyen <hnguyen33@rrc.ca>
2025-11-25 22:47:31 -0600

    fix: Add ransackable_attributes to CartItem model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :book
   belongs_to :cart
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["book_id", "cart_id", "created_at", "id", "id_value", "quantity", "updated_at"]
+  end
 end
